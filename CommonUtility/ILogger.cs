d1d5160ba4ba9599fabc2cffb08d1cb4b6e3191f
Zhijie Li <Zhijie Li>
2016-08-30 01:03:07 +0800

    add ILogger

---
+++ CommonUtility/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtility
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+
+        void LogVerbose(string message);
+
+        void LogError(string message);
+
+        void LogException(Exception ex);
+    }
+}
---
+++ CommonUtility/LoggerManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommonUtility
+{
+    public class LoggerManager
+    {
+        private List<ILogger> _loggers = new List<ILogger>();
+
+        private object _lock;
+
+        public void AddLogger(ILogger logger)
+        {
+            _loggers.Add(logger);
+        }
+
+        public void Execute(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                foreach (var logger in _loggers)
+                {
+                    logger.LogException(ex);
+                }
+            }
+
+        }
+
+        public void LogInfo(string message)
+        {
+            Execute(() =>
+            {
+                foreach (ILogger log in _loggers)
+                {
+                    log.LogInfo(message);
+                }
+            });
+        }
+    }
+}
