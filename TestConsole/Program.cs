a62f735b30e3dcd1bdf326cd53355c93ea0cbb72
Zhijie Li <ZhijieLi@outlook.com>
2016-09-18 15:13:40 +0800

    set test console as default start up project

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommonUtility;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using CommonUtility;
 
-namespace TestConsole
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            LoggerManager.Init("TestConsole");
-            LoggerManager.LogInfo("hello");
-        }
-    }
-}
+//namespace TestConsole
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //LoggerManager.Init("TestConsole");
+//            //LoggerManager.LogInfo("hello");
+
+//            Debug.WriteLine("Error in Widget 42");
+
+//            Trace.WriteLine("Error in Widget 42");
+
+//            var ts = new TraceSource("TraceTest");
+
+//            ts.TraceEvent(TraceEventType.Error, 1, "hello");
+
+//            Console.WriteLine("Press any key to exit.");
+//            Console.Read();
+//        }
+
+//    }
+//}
