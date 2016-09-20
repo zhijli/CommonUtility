54c61243d1949e0a27c5c3852bbf4cfc33589d6b
Zhijie Li <ZhijieLi@outlook.com>
2016-09-20 16:50:54 +0800

    add db logger

---
+++ CommonUtility/DbLogger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+
+namespace CommonUtility
+{
+    public class DbLogger : ILogger
+    {
+
+        public DbLogger()
+        {
+
+        }
+        public  DataSet ExecuteDb()
+        {
+            var ds = new DataSet();
+            var startTime = DateTimeOffset.UtcNow;
+            var success = false;
+
+            using (var cnn = new SqlConnection("Data Source=.;Initial Catalog=test;Integrated Security=True"))
+            using (var cmd = new SqlCommand())
+            using (var adapter = new SqlDataAdapter(cmd))
+            {
+                cmd.Connection = cnn;
+                cmd.CommandTimeout = 14400; // 4 hours = 4 * 3600 seconds
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from TraceDb";
+                cnn.Open();
+                adapter.Fill(ds);
+                success = true;
+            }
+
+            return ds;
+        }
+
+        public string LoggerName { get; set; }
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogVerbose(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogException(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommonUtilityTest/DbLoggerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommonUtility;
+using Kid.CommonUtility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonUtilityTest
+{
+    [TestClass]
+    public class DbLoggerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var db = new DbLogger();
+            var result = db.ExecuteDb();
+            Console.Write(result.DebugInfo());
+        }
+    }
+}
